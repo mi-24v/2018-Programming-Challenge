d165aa4b5fe199265c21405a9f696b42363fa9b0
mi-24v <miwpayou0808@gmail.com>
2018-03-28 11:22:42 +0900

    initialized project

---
+++ app/src/main/java/mipwayou0808/info/wheelchaircontroller/CVTestActivity.kt
@@ -0,0 +1,12 @@
+package mipwayou0808.info.wheelchaircontroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CVTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cvtest)
+    }
+}
