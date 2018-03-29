9842075a42c1e0bb4d732de766d06100248d62e1
mi-24v <miwpayou0808@gmail.com>
2018-03-29 12:06:08 +0900

    changed MAC Address real one

--- app/src/main/java/mipwayou0808/info/wheelchaircontroller/WheelChairDevice.kt
+++ app/src/main/java/mipwayou0808/info/wheelchaircontroller/WheelChairDevice.kt
@@ -6,6 +6,6 @@ import java.util.*
  * Device information
  */
 data class WheelChairDevice(
-        val macAddress: String = "AA:BB:CC:DD:FF",
+        val macAddress: String = "AC:2B:6E:33:EA:AE",
         val notificationUUID: UUID = UUID.fromString("26bd0805-3c04-4327-8441-a1e3e7c9e691"),
         val writeUUID: UUID = UUID.fromString("4eea7a79-65f3-4056-9155-4aa0ce4a53d9"))
